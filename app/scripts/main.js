748d726ee3ed926384218b8dc97762641762eee2
Dejan Vesin <vesindejan@gmail.com>
2017-12-07 19:26:03 +0100

    mobile navigation works fine, and some css and html changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function () {
+
+    $('.navigation li a').click(function () {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 106
+        }, 1200);
+        if ($(window).width() < 1024) {
+            $('.navigation').show(600).toggleClass('mobileNavigation');
+        }
+        event.preventDefault();
+        return false;
+    });
+
+    $(window).scroll(function () {
+        let scroll = $(window).scrollTop();
+        if (scroll >= 80) {
+            $('.primary').addClass('header-fixed', 600);
+        }
+        else {
+            $('.primary').removeClass('header-fixed', 600);
+        }
+    });
+
+    $('.fa-bars').click(function () {
+        event.preventDefault();
+        $('.navigation').toggleClass('mobileNavigation', 600);
+    });
+});
